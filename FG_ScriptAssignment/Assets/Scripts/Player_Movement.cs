c30cc7efaac6290b3db207561ffa2cd9db9cf61e
Corentin Haot <corentin.haot@edu.futuregames.nu>
2022-09-21 13:50:47 +0200

    Added ground/Player

---
+++ FG_ScriptAssignment/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Z))
+       {
+            transform.Translate(Vector3.forward);
+       } 
+    }
+}
